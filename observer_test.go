a1b6daa4b7b21b776fda68957314a3c08e2f69bc
Cesar Rodriguez <cesar@ooyala.com>
2015-01-07 16:23:43 -0600

    Add tests

---
+++ observer_test.go
@@ -0,0 +1,46 @@
+package observer
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+var (
+	Message = ""
+)
+
+type TestObserver struct {
+	Observer
+	SubscriptionId string
+}
+
+func (t TestObserver) Notify(value interface{}) {
+	Message = fmt.Sprintf("Received %v", value)
+}
+
+func Test(t *testing.T) {
+
+	assertEqual := func(val interface{}, exp interface{}) {
+		if val != exp {
+			t.Errorf("Expected %v, got %v.", exp, val)
+		}
+	}
+
+	publisher := Publisher{
+		Name:      "PCodeChange",
+		Observers: map[string]Observer{},
+	}
+	testObserver := TestObserver{}
+	testObserver2 := TestObserver{}
+
+	testObserver.SubscriptionId = publisher.Subscribe(testObserver)
+	testObserver2.SubscriptionId = publisher.Subscribe(testObserver2)
+
+	publisher.Unsubscribe(testObserver2.SubscriptionId)
+	publisher.Publish("test")
+
+	time.Sleep(1 * time.Second)
+
+	assertEqual(Message, "Received test")
+}
