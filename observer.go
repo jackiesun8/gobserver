0e7dad5e6ff69296cd741ef3e8808e05a4a05f52
Cesar Rodriguez <cesar@ooyala.com>
2015-01-07 16:23:33 -0600

    Initialize observer.go

---
+++ observer.go
@@ -0,0 +1,37 @@
+package observer
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type Observer interface {
+	Notify(value interface{})
+}
+
+type Publisher struct {
+	Name      string
+	Observers map[string]Observer
+}
+
+func (publisher *Publisher) Subscribe(observer Observer) string {
+	// genereate Id
+	subscriptionId := GenerateIdFromTimestamp()
+	publisher.Observers[subscriptionId] = observer
+	return subscriptionId
+}
+
+func (publisher *Publisher) Unsubscribe(subscriptionId string) {
+	delete(publisher.Observers, subscriptionId)
+}
+
+func (publisher *Publisher) Publish(value interface{}) {
+	for _, obs := range publisher.Observers {
+		go obs.Notify(value)
+	}
+}
+
+func GenerateIdFromTimestamp() string {
+	return fmt.Sprintf("%x_%x", int32(time.Now().Unix()), rand.Intn(256))
+}
